1a7334c486df3ce20fcd0f6ac483a2a96bf72652
Artem <shurtemm@mail.ru>
2023-03-11 04:47:49 +0300

    Оформили код в виде функции в задаче 19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -8,35 +8,41 @@
 
 Console.WriteLine("Введите пятизначное число:");
 int num = Convert.ToInt32( Console.ReadLine());
-int n = 10000;
-int r_num = 0;
-int l_num = 0;
-int res = 0;
-int k = 0;
-if (num < 100000 & num > 9999)
+
+Palindrom5(num);
+
+void Palindrom5(int num)
     {
-    for (int i = 1; i < 3; i++)
+    int n = 10000;
+    int r_num = 0;
+    int l_num = 0;
+    int res = 0;
+    int k = 0;
+    if (num < 100000 & num > 9999)
         {
-        res = num / n;
-        l_num = (res % 10);
-        r_num = (num % 10);
-        if (l_num > r_num | l_num < r_num)
-            k = k + 1;
-        /*
-        Console.WriteLine(num);
-        Console.WriteLine(l_num);
-        Console.WriteLine(r_num);
-        Console.WriteLine(k);
-        Console.WriteLine();
-        */
-        n = n / 100;
-        num = num /10;
+        for (int i = 1; i < 3; i++)
+            {
+            res = num / n;
+            l_num = (res % 10);
+            r_num = (num % 10);
+            if (l_num > r_num | l_num < r_num)
+                k = k + 1;
+            /*
+            Console.WriteLine(num);
+            Console.WriteLine(l_num);
+            Console.WriteLine(r_num);
+            Console.WriteLine(k);
+            Console.WriteLine();
+            */
+            n = n / 100;
+            num = num /10;
+            }
+        Console.WriteLine("Введенное число является палиндромом?");
+        if (k == 0)
+            Console.WriteLine("Да");
+        else
+            Console.WriteLine("Нет");
         }
+    else 
+        Console.WriteLine("Введенное число должно быть пятизначным");
     }
-else 
-    Console.WriteLine("Введенное число должно быть пятизначным");
-Console.WriteLine("Введенное число является палиндромом?");
-if (k == 0)
-    Console.WriteLine("Да");
-else
-    Console.WriteLine("Нет");
